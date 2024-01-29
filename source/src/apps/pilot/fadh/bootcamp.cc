fe6d2ecf4955693bcc1d0caad90f80faa752d4d1
fatimadavila <fatima.a.davila@gmail.com>
2024-01-29 17:05:52 -0600

    Added stuff to contruct a Pose object from the pdb file.

--- source/src/apps/pilot/fadh/bootcamp.cc
+++ source/src/apps/pilot/fadh/bootcamp.cc
@@ -11,15 +11,20 @@
 #include <basic/options/option.hh>
 #include <basic/options/keys/in.OptionKeys.gen.hh>
 #include <devel/init.hh>
+#include <core/import_pose/import_pose.hh>
+#include <utility/pointer/owning_ptr.hh>
+#include <core/pose/Pose.hh>
 
 int 
 main( int argc, char ** argv ) {
+    
     std::cout << "Hello World!" << std::endl;
 
     devel::init( argc, argv );
     utility::vector1< std::string > filenames = basic::options::option[ basic::options::OptionKeys::in::file::s ].value();
     if ( filenames.size() > 0 ) {
         std::cout << "You entered: " << filenames[ 1 ] << " as the PDB file to be read" << std::endl;
+        core::pose::PoseOP mypose = core::import_pose::pose_from_file( filenames[1] );
     } 
     else {
         std::cout << "You didn’t provide a PDB file with the -in::file::s option" << std::endl;
